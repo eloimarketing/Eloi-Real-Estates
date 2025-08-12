eeb2e0069f71633287f7eafc9060e4e3c04574a0
Nikhil Sharma <niikkhilsharma@gmail.com>
2025-08-13 02:11:56 +0530

    changes

--- app/api/testing/final/route.ts
+++ app/api/testing/final/route.ts
@@ -284,5 +284,5 @@ import { NextResponse } from 'next/server'
 // }
 
 export async function GET() {
-	return NextResponse.json({ name: 'John Doe' })
+	return NextResponse.json({ name: 'Testing' })
 }
